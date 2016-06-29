f6e92df391cf9aac4a3e510664b5fda049bd47dc
Yicheng Huang <395076316@qq.com>
2016-06-29 11:47:04 +0800

    add new func

--- src/share/SharedFunc.java
+++ src/share/SharedFunc.java
@@ -9,4 +9,9 @@ public class SharedFunc {
 		re = re + "//FUNCTION";
 		return re;
 	}
+	public static String GetIP(String ip) throws Exception {
+		String re = "//" + ip + ":" + Integer.toString((int)(Math.random() * 20000 + 10000));
+		re = re + "//FUNCTION";
+		return re;
+	}
 }
