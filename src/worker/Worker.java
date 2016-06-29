1ec53bcea3f0df248b1a958646ca4394f05cfb9d
Dmk112129911 <zdong@linkedin.com>
2016-06-29 11:53:22 +0800

    add worker

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -1,14 +1,21 @@
 package worker;
 
 import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+
+import share.MasterFunc;
 import share.SharedFunc;
+
 
 public class Worker {
 	 public static void main(String[] args) throws Exception {
-		 	String url = SharedFunc.GetIP();
+		 	String url = SharedFunc.GetIP("10.2.5.185");
 	        //String url = "//162.105.96.50:8804/SAMPLE-SERVER";
+		 	String serverUrl = "//162.105.96.50:8804/SAMPLE-SERVER";
+		 	MasterFunc calc = (MasterFunc) Naming.lookup(serverUrl);
+		 	calc.AddWorker(url);
 		 	System.out.println(url);
 	    }
 }
