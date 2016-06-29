313279475827953fd92e0343f31bfa56d7a8049a
Dmk112129911 <zdong@linkedin.com>
2016-06-29 11:47:23 +0800

    delete exception

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -1,22 +1,14 @@
 package worker;
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import share.SharedFunc;
 
 public class Worker {
-	 public static void main(String[] args) {
-	        String url = "//162.105.96.50:8804/SAMPLE-SERVER";
-	        try {
-	        
-	         
-	        } catch (MalformedURLException e) {
-	            e.printStackTrace();
-	        } catch (RemoteException e) {
-	            e.printStackTrace();
-	        } catch (NotBoundException e) {
-	            e.printStackTrace();
-	        }
+	 public static void main(String[] args) throws Exception {
+		 	String url = SharedFunc.GetIP();
+	        //String url = "//162.105.96.50:8804/SAMPLE-SERVER";
+		 	System.out.println(url);
 	    }
 }
