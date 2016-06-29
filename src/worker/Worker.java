77852f912b9dfbb08923c5c4dc9a4394ed9fc89e
Dmk112129911 <zdong@linkedin.com>
2016-06-29 11:35:41 +0800

    add worker

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -1,5 +1,22 @@
 package worker;
 
-public class Worker {
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
+public class Worker {
+	 public static void main(String[] args) {
+	        String url = "//162.105.96.50:8804/SAMPLE-SERVER";
+	        try {
+	        
+	         
+	        } catch (MalformedURLException e) {
+	            e.printStackTrace();
+	        } catch (RemoteException e) {
+	            e.printStackTrace();
+	        } catch (NotBoundException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
