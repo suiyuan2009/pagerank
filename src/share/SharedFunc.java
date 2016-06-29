dea0a9967d0112dda7e7c91a415d945d49abd26b
Yicheng Huang <395076316@qq.com>
2016-06-29 11:34:50 +0800

    Add Share Func.

---
+++ src/share/SharedFunc.java
@@ -0,0 +1,12 @@
+package share;
+
+import java.net.InetAddress;
+
+public class SharedFunc {
+	public static String GetIP() throws Exception {
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		String re = "//" + ip + ":" + Integer.toString((int)(Math.random() * 20000 + 10000));
+		re = re + "//FUNCTION";
+		return re;
+	}
+}
