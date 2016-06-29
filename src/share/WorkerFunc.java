2adc254bec066fbab30a3ff60247c0acd1491695
suiyuan2009 <dzm1016397507@gmail.com>
2016-06-29 19:30:29 +0800

    format worker code

--- src/share/WorkerFunc.java
+++ src/share/WorkerFunc.java
@@ -1,10 +1,12 @@
 package share;
 
-import java.rmi.Remote;  
-import java.rmi.RemoteException; 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public interface WorkerFunc extends Remote{
+public interface WorkerFunc extends Remote {
 	public int sendPrMsg(String[] workerUrls, int[] workerIds) throws Exception;
+
 	public int receivePrMsg(double pr, int idx) throws Exception;
+
 	public int calcPr() throws Exception;
 }
--- src/worker/WorkerFuncImp.java
+++ src/worker/WorkerFuncImp.java
@@ -11,27 +11,31 @@ import worker.Worker;
 
 public class WorkerFuncImp extends UnicastRemoteObject implements WorkerFunc {
 
-	public WorkerFuncImp() throws RemoteException {  
-        super();
-    }  
-	public synchronized int sendPrMsg(String[] workerUrls, int[] workerIds) throws Exception{
-    	Worker.workerUrls = workerUrls;
-    	Worker.workerIds = workerIds;
+	public WorkerFuncImp() throws RemoteException {
+		super();
+	}
+
+	public synchronized int sendPrMsg(String[] workerUrls, int[] workerIds) throws Exception {
+		Worker.workerUrls = workerUrls;
+		Worker.workerIds = workerIds;
 		Worker.sendMsgFlag = true;
-		System.out.println("master say to sendPrMsg:  "+ "workerid: "+ Worker.wpr.WorkerId );
-		return 0;
-    }
-	public synchronized int receivePrMsg(double pr, int idx) throws Exception{
-		
-		Worker.wpr.nPr.add((double)pr);
-		Worker.wpr.nids.add((int)idx);
-		System.out.println("receive Msg:  "+ "workerid: "+ Worker.wpr.WorkerId + ", node pagerank value: " + pr + ", node id: " +idx);
+		System.out.println("master say to sendPrMsg:  " + "workerid: " + Worker.wpr.WorkerId);
 		return 0;
 	}
-	public synchronized int calcPr() throws Exception{
-		
+
+	public synchronized int receivePrMsg(double pr, int idx) throws Exception {
+
+		Worker.wpr.nPr.add((double) pr);
+		Worker.wpr.nids.add((int) idx);
+		// System.out.println("receive Msg: "+ "workerid: "+ Worker.wpr.WorkerId
+		// + ", node pagerank value: " + pr + ", node id: " +idx);
+		return 0;
+	}
+
+	public synchronized int calcPr() throws Exception {
+
 		Worker.calcPrFlag = true;
-		System.out.println("master say to calc Pr:  "+ "workerid: "+ Worker.wpr.WorkerId );
+		System.out.println("master say to calc Pr:  " + "workerid: " + Worker.wpr.WorkerId);
 		return 0;
 	}
 }
